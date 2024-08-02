d85a871855bfbff73a82233ab9dff1c08dc08edb
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2024-08-02 16:11:36 +0530

    added retries for textract client

--- app/service/__init__.py
+++ app/service/__init__.py
@@ -5,4 +5,10 @@ from app.constant import AWS
 textract_client = boto3.client(
     service_name='textract',
     region_name=AWS.BotoClient.AWS_DEFAULT_REGION,
-    config=Config(AWS.BotoClient.TEXTRACT_READ_TIMEOUT))
+    config=Config(
+        read_timeout=AWS.BotoClient.TEXTRACT_READ_TIMEOUT,
+        retries={
+            'max_attempts': 5
+        }
+    )
+)
